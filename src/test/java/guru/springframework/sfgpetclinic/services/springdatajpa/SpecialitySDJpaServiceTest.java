a124ae41a9eb53dfd3195d7c1f0802271b3c461e
Winadi Wiratama <winadi.wiratama@dana.id>
2020-01-31 13:29:25 +0700

    Mockito.verify with iteration counts

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -8,6 +8,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.Mockito.*;
+
 @ExtendWith(MockitoExtension.class)
 class SpecialitySDJpaServiceTest {
     @Mock
@@ -19,6 +21,31 @@ class SpecialitySDJpaServiceTest {
     @Test
     void deleteById() {
         service.deleteById((long) 11);
+        service.deleteById((long) 11);
+        verify(specialtyRepository, times(2)).deleteById((long) 11);
+    }
+
+    @Test
+    void deleteByIdAtLeast() {
+        service.deleteById((long) 11);
+        service.deleteById((long) 11);
+        verify(specialtyRepository, atLeast(1)).deleteById((long) 11);
+    }
+
+    @Test
+    void deleteByIdAtMost() {
+        service.deleteById((long) 11);
+        service.deleteById((long) 11);
+        verify(specialtyRepository, atMost(5)).deleteById((long) 11);
+    }
+
+    @Test
+    void deleteByIdNever() {
+        service.deleteById((long) 11);
+        service.deleteById((long) 11);
+        verify(specialtyRepository, atMost(5)).deleteById((long) 11);
+
+        verify(specialtyRepository, never()).deleteById(5L);
     }
 
     @Test
